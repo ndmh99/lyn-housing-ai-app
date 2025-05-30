626825af0ed5da5ddbd95286bfb780b327e7ec80
ndmh99 <minhhieuc501@gmail.com>
2025-05-30 02:59:34 -0700

    fix: Update CORS settings and modify allowed origins in settings.py

--- backend/lynapp-django/settings.py
+++ backend/lynapp-django/settings.py
@@ -135,7 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
     "http://localhost:5173",
-    "https://your-frontend-domain.netlify.app",  # Add your actual frontend URL
 ]
